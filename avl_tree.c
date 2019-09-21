81930c654d7101ec2b44fa268f7978113d90ef43
Naaz Sibia <sibianjali@gmail.com>
2019-09-21 19:55:07 -0400

    Add AVL Tree and Linked List Structures
    This commit adds avl_tree.c and avl_tree.h files.
    
    The avl_tree.h file contains the avl structure that we'll be using to track processes.
    It also contains linked list structures that we will be using to track
    processes and file descriptors.
    
    The avl_tree.c file has functions that work with the standard avl_tree in
    addition to functions that add file descriptors and children to each process and
    a function to remove file descriptors from a process given its pid when they are
    closed

---
+++ avl_tree.c
@@ -0,0 +1,438 @@
+/**
+ *  This file contains modified code from: https://www.geeksforgeeks.org/avl-tree-set-1-insertion/. It 
+ * has functions that perform standard AVL operations in addition to adding file descriptors and a linked list
+ * of processes to each node. 
+ *
+**/
+
+#include<stdio.h> 
+#include<stdlib.h> 
+#include "avl_tree.h"
+
+
+
+/**
+ * Returns the height of the tree
+ **/
+int height(AVLNode *n) 
+{ 
+    if (n == NULL) 
+        return 0; 
+    return n->height; 
+} 
+  
+/**
+ * Returns the max of pids p1 and p2
+ * */
+int max(pid_t a, pid_t b) 
+{ 
+    return (a > b)? a : b; 
+} 
+  
+
+/**
+ * Returns a new node with the given PID
+ * and NULL left and right pointers
+ **/
+AVLNode* new_node(pid_t pid) 
+{ 
+    AVLNode* n = (AVLNode*) 
+                        malloc(sizeof(AVLNode)); 
+    if(n == NULL) return NULL; // malloc failed
+    n->pid   = pid; 
+    n->left   = NULL; 
+    n->right  = NULL; 
+    n->height = 1;  // new node is initially added at leaf 
+    return(n); 
+} 
+
+/**
+ * Right rotate tree at n
+ **/
+AVLNode *right_rotate(AVLNode *y) 
+{ 
+    AVLNode *x = y->left; 
+    AVLNode *T2 = x->right; 
+  
+    // Perform rotation 
+    x->right = y; 
+    y->left = T2; 
+  
+    // Update heights 
+    y->height = max(height(y->left), height(y->right))+1; 
+    x->height = max(height(x->left), height(x->right))+1; 
+  
+    // Return new root 
+    return x; 
+} 
+
+/**
+ * Left rotate tree at n
+**/
+AVLNode *left_rotate(AVLNode *x) 
+{ 
+    AVLNode *y = x->right; 
+    AVLNode *T2 = y->left; 
+  
+    // Perform rotation 
+    y->left = x; 
+    x->right = T2; 
+  
+    //  Update heights 
+    x->height = max(height(x->left), height(x->right))+1; 
+    y->height = max(height(y->left), height(y->right))+1; 
+  
+    // Return new root 
+    return y; 
+} 
+  
+
+/**
+ * Get the balance factor of node n
+**/
+int get_balance(AVLNode *n) 
+{ 
+    if (n == NULL) 
+        return 0; 
+    return height(n->left) - height(n->right); 
+} 
+
+  
+/**
+ * Insert node with pid p into AVL tree 
+ * rooted at n. If ppid is not -1, 
+ * add the node to ppid's node's list of children
+ * Return the new tree rooted at n 
+**/
+AVLNode* insert(AVLNode*n, pid_t pid, pid_t ppid){
+    AVLNode *new = new_node(pid);
+    if(new == NULL){
+        perror("malloc");
+        return n;  // malloc failed to make new node, so return old tree
+    }
+    AVLNode *new_tree = insert_node(n, new); // adding node to AVLTree at n 
+    if(ppid == 0) return new_tree; 
+    AVLNode *parent = search(n, ppid);
+    if(parent == NULL){
+        fprintf(stderr, "Invalid ppid"); // can help with debugging
+        return n;
+    }
+    if(add_child(parent, pid) == -2) perror("malloc");
+    new->open_fds = copy_fd_list(parent->open_fds);
+    return new_tree;
+
+
+}
+
+
+/**
+ * Return the node with pid p in AVLTree rooted at root
+**/
+AVLNode* search(AVLNode* root, pid_t p){
+    // Base Cases: root is null or key is present at root 
+    if (root == NULL || root->pid == p) 
+       return root; 
+     
+    // Key is greater than root's key 
+    if (root->pid < p) 
+       return search(root->right, p); 
+  
+    // Key is smaller than root's key 
+    return search(root->left, p); 
+}
+
+
+
+/**
+ * Insert node with node n2 into tree at node n
+**/
+AVLNode* insert_node(AVLNode* n, AVLNode *n2) 
+{ 
+    /* 1.  Perform the normal BST insertion */
+    if (n == NULL) 
+        return(n2); 
+  
+    if (n2->pid < n->pid) 
+        n->left  = insert_node(n->left, n2); 
+    else if (n2->pid > n->pid) 
+        n->right = insert_node(n->right, n2); 
+    else // Equal pids are not allowed in BST 
+        return n; 
+  
+    /* 2. Update height of this ancestor node */
+    n->height = 1 + max(height(n->left), 
+                           height(n->right)); 
+  
+    /* 3. Get the balance factor of this ancestor 
+          node to check whether this node became 
+          unbalanced */
+    int balance = get_balance(n); 
+  
+    // If this node becomes unbalanced, then 
+    // there are 4 cases 
+  
+    // Left Left Case 
+    if (balance > 1 && n2->pid < n->left->pid) 
+        return right_rotate(n); 
+  
+    // Right Right Case 
+    if (balance < -1 && n2->pid > n->right->pid) 
+        return left_rotate(n); 
+  
+    // Left Right Case 
+    if (balance > 1 && n2->pid > n->left->pid) 
+    { 
+        n->left =  left_rotate(n->left); 
+        return right_rotate(n); 
+    } 
+  
+    // Right Left Case 
+    if (balance < -1 && n2->pid < n->right->pid) 
+    { 
+        n->right = right_rotate(n->right); 
+        return left_rotate(n); 
+    } 
+  
+    /* return the (unchanged) node pointer */
+    return n; 
+} 
+
+/** 
+ * Given a non-empty binary search tree, return the 
+ * node with minimum key value found in that tree. 
+ * Note that the entire tree does not need to be 
+ * searched. 
+**/
+AVLNode * min_value_node(AVLNode* node) 
+{ 
+    AVLNode* current = node; 
+  
+    //loop down to find the leftmost leaf
+    while (current->left != NULL) 
+        current = current->left; 
+  
+    return current; 
+} 
+
+
+
+/**
+ * Deletes the node with pid p at node n are
+ * returns the root of the modified tree
+ **/
+AVLNode* delete_node(AVLNode* root, pid_t p) 
+{ 
+    // STEP 1: PERFORM STANDARD BST DELETE 
+  
+    if (root == NULL) 
+        return root; 
+  
+    // If the key to be deleted is smaller than the 
+    // root's key, then it lies in left subtree 
+    if (p < root->pid ) 
+        root->left = delete_node(root->left, p); 
+  
+    // If the key to be deleted is greater than the 
+    // root's key, then it lies in right subtree 
+    else if( p > root->pid ) 
+        root->right = delete_node(root->right, p); 
+  
+    // if key is same as root's key, then This is 
+    // the node to be deleted 
+    else
+    { 
+        // node with only one child or no child 
+        if( (root->left == NULL) || (root->right == NULL) ) 
+        { 
+            AVLNode *temp = root->left ? root->left : 
+                                             root->right; 
+  
+            // No child case 
+            if (temp == NULL) 
+            { 
+                temp = root; 
+                root = NULL; 
+            } 
+            else // One child case 
+             *root = *temp; // Copy the contents of 
+                            // the non-empty child 
+            free(temp); 
+        } 
+        else
+        { 
+            // node with two children: Get the inorder 
+            // successor (smallest in the right subtree) 
+            AVLNode* temp = min_value_node(root->right); 
+  
+            // Copy the inorder successor's data to this node 
+            root->pid = temp->pid; 
+  
+            // Delete the inorder successor 
+            root->right = delete_node(root->right, temp->pid); 
+        } 
+    } 
+  
+    // If the tree had only one node then return 
+    if (root == NULL) 
+      return root; 
+  
+    // STEP 2: UPDATE HEIGHT OF THE CURRENT NODE 
+    root->height = 1 + max(height(root->left), 
+                           height(root->right)); 
+  
+    // STEP 3: GET THE BALANCE FACTOR OF THIS NODE (to 
+    // check whether this node became unbalanced) 
+    int balance = get_balance(root); 
+  
+    // If this node becomes unbalanced, then there are 4 cases 
+  
+    // Left Left Case 
+    if (balance > 1 && get_balance(root->left) >= 0) 
+        return right_rotate(root); 
+  
+    // Left Right Case 
+    if (balance > 1 && get_balance(root->left) < 0) 
+    { 
+        root->left =  left_rotate(root->left); 
+        return right_rotate(root); 
+    } 
+  
+    // Right Right Case 
+    if (balance < -1 && get_balance(root->right) <= 0) 
+        return left_rotate(root); 
+  
+    // Right Left Case 
+    if (balance < -1 && get_balance(root->right) > 0) 
+    { 
+        root->right = right_rotate(root->right); 
+        return left_rotate(root); 
+    } 
+  
+    return root; 
+} 
+
+
+/** A utility function to print preorder traversal 
+* of the tree. 
+* The function also prints height of every node 
+**/
+void pre_order(AVLNode *root) 
+{ 
+    if(root != NULL) 
+    { 
+        printf("%d ", root->pid); 
+        pre_order(root->left); 
+        pre_order(root->right); 
+    } 
+} 
+  
+/* Drier program to test above function*/
+int main() 
+{ 
+  AVLNode *root = NULL; 
+  
+  /* Constructing tree given in the above figure */
+  root = insert(root, 10, 0); 
+  root = insert(root, 20, 0); 
+  root = insert(root, 30, 0); 
+  root = insert(root, 40, 0); 
+  root = insert(root, 50, 0); 
+  root = insert(root, 25, 0); 
+  
+  /* The constructed AVL Tree would be 
+            30 
+           /  \ 
+         20   40 
+        /  \     \ 
+       10  25    50 
+  */
+  
+  printf("Preorder traversal of the constructed AVL"
+         " tree is \n"); 
+  pre_order(root); 
+  
+  return 0; 
+} 
+/**
+ * Add a new node with the given pid to the list of children of the AVLNode with pid parent.
+ * p is in the tree at root.
+ * Returns 0 on succes, and -1 if no node with pid exists and -2 on malloc error
+**/
+int add_child_ppid(AVLNode* root, pid_t ppid, pid_t pid){
+    AVLNode *parent = search(root, ppid);
+    if(parent == NULL) return -1; 
+    return add_child(parent, pid);
+}
+
+/**
+ * Add a new node with the given pid to the list of children of parent.
+ * Returns 0 on succes, and -1 if no node with pid exists and -2 on malloc error
+**/
+int add_child(AVLNode *parent, pid_t pid){
+    ProcNode* new_child = (ProcNode*) malloc(sizeof(ProcNode)); 
+    if(new_child == NULL) return -2;
+    new_child->pid = pid;
+    ProcNode *curr = parent->child;
+    if(curr == NULL) parent->child = new_child;
+    while(curr->next != NULL) curr = curr->next; 
+    curr->next = new_child; 
+    return 0;
+}
+
+
+/**
+ * Add a new node with the given fd to the list of open file descriptors of the AVLNode with pid p.
+ * p is in the tree at root.
+ * Returns 0 on succes, and -1 if no node with pid exists and -2 on malloc error
+**/
+int add_fd(AVLNode* root, pid_t p,  int fd){
+    AVLNode *parent = search(root, p);
+    if(parent == NULL) return -1;
+    FDNode* new_fd = (FDNode*) malloc(sizeof(FDNode)); 
+    if(new_fd == NULL) return -2;
+    FDNode *curr = parent->open_fds;
+    if(curr == NULL) parent->open_fds = new_fd;
+    while(curr->next != NULL) curr = curr->next;
+    curr->next = new_fd;
+    return 0;
+}
+
+
+/**
+ * Remove node with the given fd  the list of open file descriptors the 
+ * process node with pid p has. p is in the tree at root.
+ * Return 0 on success, and -1 if the node is not found
+**/
+int remove_fd(AVLNode* root, pid_t p, int fd){
+    AVLNode *parent = search(root, p);
+    if(parent == NULL) return -1;
+    FDNode *curr = root->open_fds;
+    // no open fds
+    if(curr == NULL) return -1;
+    // head is fd
+    if(curr->fd == fd) parent->open_fds = curr->next;
+    while(curr->next != NULL){
+        if((curr->next)->fd == fd){
+         curr->next = (curr->next)->next;
+         return 0;
+        }
+        curr = curr->next;  
+    }
+    return -1; // did not find fd
+
+} 
+
+/**
+ * Returns a copy of the list at head
+**/
+FDNode* copy_fd_list(FDNode *head){
+    if(head == NULL) return head;
+    FDNode *new_head = (FDNode *) malloc(sizeof(FDNode));
+    if(new_head == NULL){
+        perror("malloc");
+        return head;
+    }
+    new_head->fd = head->fd;
+    new_head->next =  copy_fd_list(head->next);
+    return new_head;
+}
---
+++ avl_tree.h
@@ -0,0 +1,154 @@
+// for pid_t
+#include <sys/types.h>
+
+// credits: https://www.geeksforgeeks.org/avl-tree-set-2-deletion/
+
+/**
+ * LinkedList for storing the open file descriptors in 
+ * a process 
+**/
+typedef struct fd_node{
+   int fd;
+   struct fd_node *next;
+} FDNode;
+
+typedef struct process_node{
+    int pid;
+    struct process_node *next; 
+} ProcNode;
+
+/**
+ *  Binary Search Tree for storing information about a single
+ * process. Each tree node contains contains a list of children. 
+ * The processes are stored in order of their PIDs. Nodes with
+ * smaller PIDs than the root are stored in the left subtree while
+ * nodes with bigger PIDs than the root are stored in the right subtree.
+ * This recursively applies to all the trees in the BST. 
+ **/ 
+// An AVL tree node 
+typedef struct node 
+{ 
+    int pid; 
+    struct node *left; 
+    struct node *right; 
+    struct node *next; // helps store node in list
+    //int num_children; // number of child processes
+    ProcNode *child; // list of children
+    FDNode *open_fds; // list of open fds
+    int height; // important for AVL functions
+} AVLNode; 
+
+
+
+/**
+ * Returns the max of pids p1 and p2
+ * */
+int max(pid_t p1, pid_t p2); 
+
+/**
+ * Returns the height of the tree
+ **/
+int height(AVLNode *n);
+
+/**
+ * Returns a copy of the list at head
+**/
+FDNode* copy_fd_list(FDNode *head);
+
+/**
+ * Returns a new node with the given PID
+ * and NULL left and right pointers
+ **/
+AVLNode* new_node(int key); 
+
+/**
+ * Right rotate tree at n
+ **/
+AVLNode* right_rotate(AVLNode *n);
+
+/**
+ * Left rotate tree at n
+**/
+AVLNode *left_rotate(AVLNode *n);
+
+/** 
+ * Given a non-empty binary search tree, return the 
+ * node with minimum key value found in that tree. 
+ * Note that the entire tree does not need to be 
+ * searched. 
+**/
+AVLNode * min_value_node(AVLNode* node);
+
+/**
+ * Deletes the node with pid p at node n are
+ * returns the root of the modified tree
+ **/
+AVLNode* delete_node(AVLNode* root, pid_t p);
+
+
+/**
+ * Get the balance factor of node n
+**/
+int get_balance(AVLNode *n);
+
+/**
+ * Return the node with min value at tree rooted
+ * at node n
+**/
+AVLNode * min_value_node(AVLNode* n);
+
+/**
+ * Insert node with pid p into AVL tree 
+ * rooted at n. If Parent node p is not NULL, 
+ * add the node to p's list of children
+ * Return the new tree rooted at n 
+**/
+AVLNode* insert(AVLNode*n, pid_t pid, pid_t ppid);
+
+
+/**
+ * Return the node with pid p in AVLTree rooted at root
+**/
+AVLNode* search(struct node* root, pid_t p);
+
+/**
+ * Insert node with node n2 into tree at node n
+**/
+AVLNode* insert_node(AVLNode* n, AVLNode *n2);
+
+/**
+ * Delete node with pid p in tree rooted at
+ * node n
+**/
+AVLNode* delete_node(AVLNode* n, pid_t p);
+
+/**
+ * Add a new node with the given pid to the list of children of the AVLNode with pid parent.
+ * p is in the tree at root.
+ * Returns 0 on succes, and -1 if no node with pid exists and -2 on malloc error
+**/
+int add_child_ppid(AVLNode* root, pid_t ppid, pid_t pid);
+
+
+/**
+ * Add a new node with the given pid to the list of children of parent.
+ * Returns 0 on succes, and -1 if no node with pid exists and -2 on malloc error
+**/
+int add_child(AVLNode *parent, pid_t pid);
+/**
+ * Add FDnode f to the list of open file descriptors of node with pid p
+ * in the tree at the given root,
+ * Returns 0 on succes, and -1 on malloc error
+**/
+int add_fd(AVLNode* root, pid_t p,  int fd);
+
+/**
+ * Remove node with the given fd  from node n's list of open file descriptors.
+ * Return 0 on success, and -1 if the node is not found
+**/
+int remove_fd(AVLNode* root, pid_t p, int fd); 
+
+/**
+ * Prints preorder traversal of tree at root n - helps debug
+**/
+void pre_order(AVLNode *n); 
